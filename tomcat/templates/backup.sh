67aaa4f63ed935b87f407a39a0523454889bc36d
rk365040 <rknaidu08@gmail.com>
2021-10-11 21:46:44 +0530

    Update backup.sh

--- tomcat/templates/backup.sh
+++ tomcat/templates/backup.sh
@@ -5,18 +5,10 @@ BACKUP_DIR="/home/ram/backups/$BACKUP_DATE"
 
 HOSTNAME=`hostname'
 
-if [ "$HOSTNAME" = "ip-172.31.92.27" -o "$HOSTNAME" = "ip-172.31.88.51" ]; then
-	backup="dev"
-else
-  echo "server not found"
-fi
-
 FILE="$BACKUP_DATE.tar.gz"
 mkdir -p $BACKUP_DIR
-echo "create backup directory"
 
-if [ -d $BACKUP_DIR ]; then
-	tar -cvf $FILE $BACKUP_PATH
-else
-   echo "BACKUP_DIR : $BAKUP_DIR does not exist"
-fi
+echo "create backup directory"
+cd $BACKUP_DIR
+tar -cvf $FILE $BACKUP_PATH
+
